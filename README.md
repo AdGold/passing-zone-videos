75811a1f02a3a26325455402fef97e87e8dc458d
Adrian Goldwaser <ad.goldwaser@gmail.com>
2024-08-14 17:48:13 +0100

    Split into music/no music melt files and start using a config file + minor cleanups

--- README.md
+++ README.md
@@ -20,13 +20,19 @@ git clone https://github.com/AdGold/passing-zone-videos.git
 ## Usage
 
 Create a folder with the following files
-* `title.txt`: the name of the patter
+* `config`: config file with the following fields:
+  * `TITLE="Title"`: the title of the video
+  * `AUDIO_DURATION=10` (optional): the duration of the audio clip in seconds (defaults to just under the total video duration)
 * `notation.tex`: the notation and credits as a latex document
 * `video.mp4`: the video clip
 * `audio.mp3` (optional): the music to add
 * `project.melt` (optional): the melt file to define the project if non-standard (can normally be left out)
 
-Run `./render.sh <folder name>`
+Existence of an `audio.mp3` file will add music to the video and use a slightly different default melt file which mutes the audio of the video clip.
+
+Run `./render.sh <folder name> [-p]`
+
+The `-p` flag will preview the video in `melt` instead of rendering it.
 
 This will create two files:
 * `<title>.mp4` - the output video
--- render.sh
+++ render.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
 
-# Required setup:
-# sudo apt install texlive-xetex imagemagick ffmpeg melt
-# mkdir -p ~/.fonts
-# cp common/*.ttf ~/.fonts
-# fc-cache -f -v
-
 FOLDER="$1"
+PREVIEW=$(test "$2" = "-p" && echo "1")
 cd $FOLDER
-TITLE="$(cat title.txt)"
+. config
 COMMON="../common"
 
-# Get the notation as a PNG
+# Get the notation/credits as a PNGs
+echo $TITLE > title.txt
 xelatex notation.tex
 convert -density 500 -quality 100 notation.pdf notation.png
 mv notation-0.png notation.png
 mv notation-1.png credits.png
-rm notation.aux notation.log notation.pdf
+rm notation.{aux,log,pdf} title.txt
 
 # Overlay title on intro
-# Check if name contains an accent - if so we can't use Obelix so use a similar font
-if [[ $TITLE == *[äöüàèìòùáéíóú]* ]]; then
-    FONT="$COMMON/bangers.regular.ttf"
-else
-    FONT="$COMMON/ObelixProB-cyr.ttf"
-fi
+FONT="$COMMON/ObelixProB-cyr.ttf"
+[[ $TITLE =~ [äöüàèìòùáéíóú] ]] && FONT="$COMMON/bangers.regular.ttf" # Obelix doesn't support accents
 ffmpeg -i $COMMON/PZ-INTRO-without-pattern-name.avi -vf "drawtext=fontfile=$FONT: enable='gte(t,1.5)': text='$TITLE': fontcolor=white: fontsize=80: x=(w-text_w)/2: y=(h-text_h-80) + (text_h+80)*(2.5-min(t\,2.5))" -c:a copy -y PZ-intro.mp4
 
 # Trim and fade audio.mp3 here because melt needs exact times
 if [ -f audio.mp3 ]; then
-    duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 video.mp4)
-    # Intro is 5s, notation and credits are 4s each, minus some overlaps
-    audio_duration=$(echo $duration + 11 | bc)
+    # Use custom audio duration if provided, otherwise calculate it as video duration + 9s (notation + credits + part of intro)
+    audio_duration=${AUDIO_DURATION:-$(echo $(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 video.mp4) + 9 | bc)}
     non_fade=$(echo $audio_duration - 2 | bc)
     ffmpeg -i audio.mp3 -t $audio_duration -acodec copy -y trimmed_audio.mp3
     ffmpeg -i trimmed_audio.mp3 -af "afade=t=out:st=$non_fade:d=2" -y final_audio.mp3
     rm trimmed_audio.mp3
+    MELT="$COMMON/project-music.melt"
 else
-    touch final_audio.mp3
+    MELT="$COMMON/project-nomusic.melt"
 fi
+[ -f project.melt ] && MELT="project.melt"
 
 # Combine using melt
-if [ -f project.melt ]; then
-    MELT="project.melt"
+if [ -n "$PREVIEW" ]; then
+    melt melt_file:$MELT 
 else
-    MELT="$COMMON/project.melt"
+    melt -quiet melt_file:$MELT -consumer "avformat:$TITLE.mp4" acodec=libmp3lame vcodec=libx264 b=12000k quality=high+ width=1920 height=1080 preset=slow profile=high crf=18 threads=4
 fi
-melt -quiet melt_file:$MELT -consumer avformat:output.mp4 acodec=libmp3lame vcodec=libx264 b=12000k quality=high+ width=1920 height=1080 preset=slow profile=high crf=18
 rm PZ-intro.mp4 credits.png final_audio.mp3
-mv output.mp4 "$TITLE.mp4"
 mv notation.png "$TITLE - notation.png"
-
